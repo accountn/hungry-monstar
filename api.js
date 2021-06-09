531499e55d59a5dbef36bfa4b4b9065a07f2694c
helal <helalimage12345@gmail.com>
2021-06-09 23:20:57 +0600

    second commit

--- api.js
+++ api.js
@@ -1,19 +1,38 @@
 
-fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-.then(res => res.json())
-.then(data => getData(data))
+function getFoodInfo(inputValue) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${inputValue}`)
+        .then(res => res.json())
+        .then(data => getData(data))
 
-const getData = data => {
-    const item = data.categories;
-    for (let i = 0; i < item.length; i++) {
-        const element = item[i];
-        console.log(element);
-        
-    }
 }
 
-    
+
+document.getElementById("search_button").addEventListener("click", function () {
+    const inputValue = document.getElementById("searchProduct").value;
+    getFoodInfo(inputValue);
+});
+
+
+const getData = data => {
+    const item = data.meals;
+    console.log(item);
+    const divInfo = document.getElementById("productInfo");
+    item.forEach(element => {
+        // console.log(element.strCategory);
+        const div = document.createElement('div');
+        div.className = 'imgSize';
+        const divDetail = `
+            <img width: '30' src="${element.strMealThumb}">
+            <h4>${element.strMeal}</h4>
+            
+        `;
+        div.innerHTML = divDetail;
+        divInfo.appendChild(div);
+
+    });
+}
 
 
 
-    
+
+
